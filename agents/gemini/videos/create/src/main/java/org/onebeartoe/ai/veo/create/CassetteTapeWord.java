c463021b5842965772ce1a5139ededbce6f34a57
Roberto Marquez <onebeartoe@gmail.com>
2026-03-31 21:51:32 -0500

    add a main method for Robot Chicken Video

--- agents/gemini/videos/create/src/main/java/org/onebeartoe/ai/veo/create/CassetteTapeWord.java
+++ agents/gemini/videos/create/src/main/java/org/onebeartoe/ai/veo/create/CassetteTapeWord.java
@@ -39,5 +39,5 @@ public class CassetteTapeWord extends CreateVideo
                 .prompt(promptText)                
                 .image(tapeTextImage)
                 .build();
-    }            
+    }
 }
--- agents/gemini/videos/create/src/main/java/org/onebeartoe/ai/veo/create/RobotChickenVideoFromTextAndImage.java
+++ agents/gemini/videos/create/src/main/java/org/onebeartoe/ai/veo/create/RobotChickenVideoFromTextAndImage.java
@@ -1,20 +1,16 @@
 //TODO: make this a main class or jbang!
 package org.onebeartoe.ai.veo.create;
 
+import com.google.genai.types.GenerateVideosSource;
 import com.google.genai.types.Image;
-
-//TODO:
-//TODO:
-//TODO: REfactor this to use just a main class 
-//TODO:
-//TODO:
-
+import org.onebeartoe.prompts.Prompts;
 
 /**
  *
  */
-public class RobotChickenVideoFromTextAndImage 
+public class RobotChickenVideoFromTextAndImage extends CreateVideo
 {
+//TODO: move this to Prompts.java    
             String prompt = """
                         use the robot chicken as a 
                             model for a cute futuristic pet
@@ -25,5 +21,24 @@ public class RobotChickenVideoFromTextAndImage
         
         // Define the input images as Content parts        
         Image image = Image.fromFile("robot-chicken-b.png", "image/png");
-    
+
+    public static void main(String[] args) throws InterruptedException 
+    {
+        System.out.println("Hello from RobotChickenVideoFromTextAndImage!");
+        
+        var app = new RobotChickenVideoFromTextAndImage();
+        
+        app.fromText();        
+    }        
+        
+    @Override
+    public GenerateVideosSource videosSource() 
+    {
+        var promptText = Prompts.Imaging.FunkyElectricChickenLeg.LITERAL;
+        
+        return GenerateVideosSource.builder()
+                .prompt(promptText)                
+                .image(image)
+                .build();
+    }        
 }
