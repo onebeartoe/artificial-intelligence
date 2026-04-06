cb58cd28ae77785ec4b82ae3679ec0a343820606
Roberto Marquez <onebeartoe@gmail.com>
2026-04-06 18:55:45 -0500

    use the correct response

--- agents/gemini/videos/create/src/main/java/org/onebeartoe/ai/veo/create/HowSillyCanYouGetVideo.java
+++ agents/gemini/videos/create/src/main/java/org/onebeartoe/ai/veo/create/HowSillyCanYouGetVideo.java
@@ -4,41 +4,40 @@ package org.onebeartoe.ai.veo.create;
 import com.google.genai.types.GenerateVideosSource;
 import com.google.genai.types.Image;
 import org.onebeartoe.prompts.Prompts;
+import static org.onebeartoe.prompts.Prompts.Music.Silly.HOW_SILLY_CAN_YOU_GET_LYRICS;
 
 /**
  *
  */
-public class RobotChickenVideoFromTextAndImage extends CreateVideo
-{
-//TODO: move this to Prompts.java    
-            String prompt = """
-                        use the robot chicken as a 
-                            model for a cute futuristic pet
-                                    waling around nice park.
-                                            add flying cars in the         city skyline. 
-                                                    The video should have a cinematic feel.
-                                                                                           """;
-        
-        // Define the input images as Content parts        
-        Image image = Image.fromFile("robot-chicken-b.png", "image/png");
+public class HowSillyCanYouGetVideo extends CreateVideo
+{      
+    // Define the input images as Content parts        
+    Image image = Image.fromFile("/home/luke/Versioning/owner/github/artificial-intelligence/agents/gemini/images/create/surf-folks-2026-04-06-1533-a.png", "image/png");
 
     public static void main(String[] args) throws InterruptedException 
     {
-        System.out.println("Hello from RobotChickenVideoFromTextAndImage!");
+        System.out.println("Hello from HowSillyCanYouGetVideo!");
         
-        var app = new RobotChickenVideoFromTextAndImage();
+        var app = new HowSillyCanYouGetVideo();
         
-        app.fromText();        
+        app.fromText();      
+        
+        System.out.println("sillines video done");
     }        
         
     @Override
     public GenerateVideosSource videosSource() 
     {
-        var promptText = Prompts.Imaging.FunkyElectricChickenLeg.LITERAL;
+        var promptText = HOW_SILLY_CAN_YOU_GET_LYRICS + 
+                """
+                Do not show the lyrics on the video.
+                use the audio form this file: ../../music/silly-surf-song-2026-04-06-1611.mp3
+                """;
         
         return GenerateVideosSource.builder()
                 .prompt(promptText)                
                 .image(image)
+                
                 .build();
     }        
 }
